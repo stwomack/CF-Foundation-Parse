846ec103a2e343b9504159f48fd4cb8e905ef6db
Administrator <admin@example.com>
2017-08-22 11:02:04 -0500

    Updates script

--- parse.py
+++ parse.py
@@ -2,34 +2,51 @@
 
 import json
 
+foundation_desc = {"ps2pcf02":"ProdInternal", "ps2pcf01":"Non-ProdSandbox",
+                   "ch3pcf05":"UATExternal", "ps2pcf03":"ProdExternal",
+                   "ch3pcf03":"ProdInternalDR", "ch3pcf04":"PCF System Apps (Jenkins, Artifactory)",
+                   "ch3pcf01":"NonProd Dev/QA/UAT Internal"}
+
 with open('status_orig.json') as data_file:
     data = json.load(data_file)
 
-csv=[]
-total_ai=0
-total_si=0
-#
-# def totals():
-#     for i in data:
-#         foundation = i.keys()[0]
-#         orgs = i[foundation]['orgs'].keys()
-#         total_ai_in_foundation=0
-#         total_ai_in_foundation += i[foundation]["total_app_instances"]
-#         total_ai += total_ai_in_foundation
-#         total_sis_in_foundation=0
-#         for org in orgs:
-#             spaces = i[foundation]['orgs'][org]['spaces'].keys()
-#             for space in spaces:
-#                 sis = i[foundation]['orgs'][org]['spaces'][space]["service_instances"]
-#                 si_size = len (sis)
-#                 if si_size > 0:
-#                     total_sis_in_foundation += si_size
-#         total_si += total_sis_in_foundation
-#         print "Total AI in: " + foundation + ": " + str(total_ai_in_foundation)
-#         print "Total SI's in: " + foundation + ": " + str(total_sis_in_foundation)
-#         print ""
-#     print "Total AIs: " + str(total_ai)
-#     print "Total SIs: " + str(total_si)
+def totals():
+    total_apps=0
+    total_users=0
+    total_ai=0
+    total_si=0
+    for i in data:
+        foundation = i.keys()[0]
+        orgs = i[foundation]['orgs'].keys()
+        total_ai_in_foundation=0
+        total_apps_in_foundation=0
+        total_users_in_foundation=0
+        total_ai_in_foundation += i[foundation]["total_app_instances"]
+        total_apps_in_foundation += i[foundation]["total_apps"]
+        total_users_in_foundation += i[foundation]["total_users"]
+        total_ai += total_ai_in_foundation
+        total_sis_in_foundation=0
+        for org in orgs:
+            # if org not in ['system', 'p-spring-cloud-services']:
+            spaces = i[foundation]['orgs'][org]['spaces'].keys()
+            for space in spaces:
+                sis = i[foundation]['orgs'][org]['spaces'][space]["service_instances"]
+                si_size = len (sis)
+                if si_size > 0:
+                    total_sis_in_foundation += si_size
+        total_apps += total_apps_in_foundation
+        total_si += total_sis_in_foundation
+        total_users += total_users_in_foundation
+        foundation_description = foundation_desc[foundation]
+        print "Total AIs " + foundation + ": " + foundation_description + ": " + str(total_ai_in_foundation)
+        print "Total Apps " + foundation + ": " +foundation_description + ": " + str(total_apps_in_foundation)
+        print "Total SIs " + foundation + ": " +foundation_description + ": " + str(total_sis_in_foundation)
+        print "Total Users " + foundation + ": " +foundation_description + ": " + str(total_users_in_foundation)
+        print ""
+    print "Total AIs: " + str(total_ai)
+    print "Total APPs: " + str(total_apps)
+    print "Total SIs: " + str(total_si)
+    print "Total Users: " + str(total_users)
 
 def print_csv():
     for i in data:
@@ -39,8 +56,8 @@ def print_csv():
             spaces = i[foundation]['orgs'][org]['spaces'].keys()
             for space in spaces:
                 for app in i[foundation]['orgs'][org]['spaces'][space]['apps']:
-                  if org not in ['system', 'p-spring-cloud-services']:
-                    print foundation + "," + org + "," + space + "," + app
+                  # if org not in ['system', 'p-spring-cloud-services']:
+                  print foundation + "," + org + "," + space + "," + app
     return
 
-print_csv()
+totals()
